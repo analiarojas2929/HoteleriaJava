db2ef29351a29a476ba65998880b4e32cec5dcf4
analiarojas2929 <analia.rojas@duocuc.cl>
2025-02-26 11:47:37 -0300

    commits

--- src/controlador/Conexion.java
+++ src/controlador/Conexion.java
@@ -1,9 +1,6 @@
 package controlador;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 
 import java.sql.Connection;
